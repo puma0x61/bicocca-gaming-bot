8f31966bef7bc8626d8858ee1fd323c9524beaef
PumaConcolor <puma.9789@gmail.com>
2022-11-28 12:51:05 +0100

    Small Changes

--- bicocca_gaming_bot/core/constants.py
+++ bicocca_gaming_bot/core/constants.py
@@ -1,18 +1,6 @@
 WELCOME_MESSAGE = f'Qui potrai parlare di gaming e gioco di ruolo e fare amicizia con altri giocatori.\n\n' \
                   f'Dicci qualcosa di te, a cosa giochi?'
 
-# LINK_GRUPPI = '<a href="https://t.me/bicoccaGamingeSportsLoL"></a>' \
-#              '<a href="https://t.me/BicoccaGamingPokemon></a>' \
-#              '<a href="https://t.me/bicoccaValorant></a>' \
-#              '<a href="https://t.me/BicoccaGamingMagic></a>' \
-#              '<a href="https://t.me/bicoccaTTRPGGaming></a>' \
-#              '<a href="https://discord.com/invite/TnmmhrgFab></a>' \
-#              '<a href="https://www.twitch.tv/bicoccagaming></a>' \
-#              '<a href="https://www.instagram.com/bicocca.gaming/?hl=it></a>' \
-#              '<a href="https://steamcommunity.com/groups/BicoccaGaming></a>'
-
-# LINKTREE = 'https://linktr.ee/bicoccagaming'
-
 LINK_MESSAGE = 'Ciao! Ecco i link ai nostri gruppi telegram e social vari\n\n' \
                '<a href=\"https://t.me/bicoccaGamingeSportsLoL\">LoL</a>\n' \
                '<a href=\"https://t.me/BicoccaGamingPokemon\">Pokémon</a>\n' \
